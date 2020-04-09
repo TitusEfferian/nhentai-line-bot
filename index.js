6b12beda05d81ac8525c5c421b8570c7f16969f8
titusefferian <titus.efferian@tokopedia.com>
2020-04-10 00:22:32 +0700

    efferian counter

--- index.js
+++ index.js
@@ -7,23 +7,29 @@ const config = {
 };
 
 const app = express();
+
+const client = new line.Client(config);
+
+const handleEvent = (event) => {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    if (event.message.type === 'text' && event.message.text.toLowerCase().includes('efferian')) {
+        return client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: 'efferian detected, database counter + 1',
+        });
+    }
+
+    return Promise.resolve(null);
+}
+
 app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result));
 });
-
-const client = new line.Client(config);
-function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
-    }
-
-    return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: event.message.text
-    });
-}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
