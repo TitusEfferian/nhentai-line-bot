0baace5008b3b5899c4c22e8b8a9e1b089a8a715
titusefferian <titus.efferian@tokopedia.com>
2020-04-10 11:17:35 +0700

    handle get top player

--- controller/handleGetTopPlayer.js
+++ controller/handleGetTopPlayer.js
@@ -3,7 +3,7 @@ const db = require('../DatabaseConnector');
 const handleGetTopPlayer = (resultCallback) => {
     const docRef = db.collection('TopEfferianMoment').orderBy("efferian_points", "desc").limit(10);
     const getUserData = docRef.get().then(doc => {
-        if (!doc.exists) {
+        if (doc.exists) {
             resultCallback(doc.data());
         }
     }).catch(err => {
