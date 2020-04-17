87bc2eafcfc9c29cfffb3a8c6778f525744092cf
titusefferian <titus.efferian@tokopedia.com>
2020-04-17 20:31:26 +0700

    handle sanitize params

--- controller/handleNhentaiSearch.js
+++ controller/handleNhentaiSearch.js
@@ -4,10 +4,17 @@ const nhentaiSearchBypass = process.env.NHENTAI_SEARCH_BYPASS.toString();
 const nhentaiSearchCrawler = process.env.NHENTAI_SEARCH_CRAWLER.toString();
 
 const handleNhentaiSearch = async (searhParams, client, replyToken) => {
-    const fetchSearchCrawler = await fetch(nhentaiSearchCrawler + '?search=suguha');
+    const searchSanitize = searhParams.replace(' ', '%20');
+    const fetchSearchCrawler = await fetch(nhentaiSearchCrawler + '?search=' + searchSanitize);
     const searchResult = await fetchSearchCrawler.json();
     const arrayOfResult = searchResult.arrayOfResult;
     const arrayOfColumns = [];
+    if (arrayOfResult.length === 0) {
+        return client.replyMessage(replyToken, {
+            "type": "text",
+            "text": "no result for " + searhParams,
+        });
+    }
     const handleValidateIncrement = () => {
         if (arrayOfResult.length > 10) {
             return 10;
