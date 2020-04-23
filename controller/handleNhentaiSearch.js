9262a1591b4dafcdb9be72d51dd87f30c083c1b5
titusefferian <titus.efferian@tokopedia.com>
2020-04-23 18:56:24 +0700

    nhentai search try catch

--- controller/handleNhentaiSearch.js
+++ controller/handleNhentaiSearch.js
@@ -4,43 +4,51 @@ const nhentaiSearchBypass = process.env.NHENTAI_SEARCH_BYPASS.toString();
 const nhentaiSearchCrawler = process.env.NHENTAI_SEARCH_CRAWLER.toString();
 
 const handleNhentaiSearch = async (searhParams, client, replyToken) => {
-    const searchSanitize = searhParams.replace(' ', '%20');
-    const fetchSearchCrawler = await fetch(nhentaiSearchCrawler + '?search=' + searchSanitize);
-    const searchResult = await fetchSearchCrawler.json();
-    const arrayOfResult = searchResult.arrayOfResult;
-    const arrayOfColumns = [];
-    if (arrayOfResult.length === 0) {
+    try {
+        const searchSanitize = searhParams.replace(' ', '%20');
+        const fetchSearchCrawler = await fetch(nhentaiSearchCrawler + '?search=' + searchSanitize);
+        const searchResult = await fetchSearchCrawler.json();
+        const arrayOfResult = searchResult.arrayOfResult;
+        const arrayOfColumns = [];
+        if (arrayOfResult.length === 0) {
+            return client.replyMessage(replyToken, {
+                "type": "text",
+                "text": "no result for " + searhParams,
+            });
+        }
+        const handleValidateIncrement = () => {
+            if (arrayOfResult.length > 10) {
+                return 10;
+            }
+            return arrayOfResult.length;
+        }
+        for (let a = 1; a <= handleValidateIncrement(); a++) {
+            arrayOfColumns.push(
+                {
+                    "imageUrl": nhentaiSearchBypass + "?url=" + arrayOfResult[a - 1].preview,
+                    "action": {
+                        "type": "postback",
+                        "label": arrayOfResult[a - 1].nhentai_id,
+                        "data": "bypass",
+                    }
+                }
+            )
+        }
+        return client.replyMessage(replyToken, {
+            "type": "template",
+            "altText": "nhentai results",
+            "template": {
+                "type": "image_carousel",
+                "columns": arrayOfColumns,
+            }
+        });
+    } catch (err) {
         return client.replyMessage(replyToken, {
             "type": "text",
-            "text": "no result for " + searhParams,
+            "text": JSON.stringify(err),
         });
     }
-    const handleValidateIncrement = () => {
-        if (arrayOfResult.length > 10) {
-            return 10;
-        }
-        return arrayOfResult.length;
-    }
-    for (let a = 1; a <= handleValidateIncrement(); a++) {
-        arrayOfColumns.push(
-            {
-                "imageUrl": nhentaiSearchBypass + "?url=" + arrayOfResult[a - 1].preview,
-                "action": {
-                    "type": "postback",
-                    "label": arrayOfResult[a - 1].nhentai_id,
-                    "data": "bypass",
-                }
-            }
-        )
-    }
-    return client.replyMessage(replyToken, {
-        "type": "template",
-        "altText": "nhentai results",
-        "template": {
-            "type": "image_carousel",
-            "columns": arrayOfColumns,
-        }
-    });
+    
 };
 
 module.exports = handleNhentaiSearch;
