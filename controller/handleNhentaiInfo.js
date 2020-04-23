7f3570f2f965a35480ae0fc7a8a5684958d91a7b
almagustaf <32619119+almagustaf@users.noreply.github.com>
2020-04-23 08:51:33 +0700

    trakir bro (#4)
    * add command, edit typo
    
    * fixing
    
    * update
    
    * string -> should be boolean
    
    * update
    
    * fixing lagi
    
    * edit title, spacing
    
    Co-authored-by: Titus Efferian <kiritosato@gmail.com>
    Co-authored-by: Titus Efferian <titus.efferian@tokopedia.com>

--- controller/handleNhentaiInfo.js
+++ controller/handleNhentaiInfo.js
@@ -8,13 +8,15 @@ const handleNhentaiInfo = async (searchParams, client, replyToken) => {
     const resultData = infoResult.data;
     const tagsData = resultData.tags;
 
-    const titleData = resultData.title;
 
-    const tags = tagsData.filter(x=>x.type === 'tag') || [];
-    const language = tagsData.filter(x=>x.type === 'language') || []; 
-    const artist = tagsData.filter(x=>x.type === 'artist') || [];
-    const parody = tagsData.filter(x=>x.type === 'parody') || [];
-    const character = tagsData.filter(x=>x.type === 'character') || [];
+    const titleData = resultData.filter(x=>x.type === 'title');
+
+    const tags = tagsData.filter(x=>x.type === 'tag');
+    const language = tagsData.filter(x=>x.type === 'language');
+    const artist = tagsData.filter(x=>x.type === 'artist');
+    const parody = tagsData.filter(x=>x.type === 'parody');
+    const character = tagsData.filter(x=>x.type === 'character');
+
 
     if (resultData.error === true) {
         return client.replyMessage(replyToken, {
@@ -31,14 +33,14 @@ const handleNhentaiInfo = async (searchParams, client, replyToken) => {
 
     return client.replyMessage(replyToken, {
         type: 'text',
-        text: `info for this code ` + searchParams + `\n\n
-        Title : `+ 'lagi di debug alma' +`\n
-        Language : `+ language.map(x=>x.name).toString() +`\n
-        Artist : `+ artist.map(x=>x.name).toString() +`\n
-        Parody : `+ parody.map(x=>x.name).toString() +`\n
-        Character : `+ character.map(x=>x.name).toString() +`\n
-        Tags : `+ tags.map(x=>x.name).toString() +`\n
-        `
+        text: `info for this code ` + searchParams + `\n`+
+        `Title : `+ titleData.map(x=>x.english).toString() +`\n`+
+        `Language : `+ language.map(x=>x.name).toString() +`\n`+
+        `Artist : `+ artist.map(x=>x.name).toString() +`\n`+
+        `Parody : `+ parody.map(x=>x.name).toString() +`\n`+
+        `Character : `+ character.map(x=>x.name).toString() +`\n`+
+        `Tags : `+ tags.map(x=>x.name).toString() +`\n`
+
     });
 };
 
