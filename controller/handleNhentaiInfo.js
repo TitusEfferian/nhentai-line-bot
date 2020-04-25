f1dec83fb0113c465827778ca2aa958b3a8c1f1b
almagustaf <32619119+almagustaf@users.noreply.github.com>
2020-04-25 14:45:07 +0700

    error handle when entered code is gibberish (#6)
    * add command, edit typo
    
    * fixing
    
    * update
    
    * string -> should be boolean
    
    * update
    
    * fixing lagi
    
    * edit title, spacing
    
    * fix title
    
    * error handle for when entered code is gibberish shit
    
    * add condition and handler error
    
    * update parsejson, update tambah spasi untuk tags, tambah newline
    
    Co-authored-by: Titus Efferian <kiritosato@gmail.com>
    Co-authored-by: Titus Efferian <titus.efferian@tokopedia.com>

--- controller/handleNhentaiInfo.js
+++ controller/handleNhentaiInfo.js
@@ -5,41 +5,44 @@ const nhentaiGetInfo = process.env.NHENTAI_SEARCH_INFO.toString();
 const handleNhentaiInfo = async (searchParams, client, replyToken) => {
     const fetchGethInfo = await fetch(nhentaiGetInfo + '?id=' + searchParams);
     const infoResult = await fetchGethInfo.json();
-    const resultData = infoResult.data;
-    const tagsData = resultData.tags;
     const parsedJson = JSON.parse(JSON.stringify(infoResult));
-
+    const resultData = parsedJson.data;
     const titleData = parsedJson.data.title;
-    const tags = tagsData.filter(x=>x.type === 'tag');
+    const tagsData = resultData.tags;
+    
+    const tags = tagsData.filter(x=>x.type == 'tag');
     const language = tagsData.filter(x=>x.type === 'language');
     const artist = tagsData.filter(x=>x.type === 'artist');
     const parody = tagsData.filter(x=>x.type === 'parody');
     const character = tagsData.filter(x=>x.type === 'character');
 
-    if (resultData.error === true) {
+    
+	if(infoResult.success === true ){
         return client.replyMessage(replyToken, {
-            "type": "text",
-            "text": "no info found for this code " + searhParams,
+
+            type: 'text',
+            text: `info for this code ` + searchParams + `\n\n`+
+            `Title : `+ titleData.english +`\n`+
+            `Language : `+ language.map(x=>x.name).toString() +`\n\n`+
+            `Artist : `+ artist.map(x=>x.name).toString() +`\n\n`+
+            `Parody : `+ parody.map(x=>x.name).toString() +`\n\n`+
+            `Character : `+ character.map(x=>x.name).toString() +`\n\n`+
+            `Tags : `+ tags.map(x=>x.name).join(', ').toString() +`\n\n`
+    
         });
-    }else if(infoResult.success === false){
+        
+    }else if (infoResult.success === false || resultData.error === true) {
         return client.replyMessage(replyToken, {
             "type": "text",
-            "text": "no info found for this code " + searhParams,
+            "text": "no info found for this search term " + searhParams,
+        });
+
+    }else{
+        return client.replyMessage(replyToken, {
+            "type": "text",
+            "text": "Invalid input " + searhParams,
         });
     }
-    
-
-    return client.replyMessage(replyToken, {
-        type: 'text',
-        text: `info for this code ` + searchParams + `\n`+
-        `Title : `+ titleData.english +`\n`+
-        `Language : `+ language.map(x=>x.name).toString() +`\n`+
-        `Artist : `+ artist.map(x=>x.name).toString() +`\n`+
-        `Parody : `+ parody.map(x=>x.name).toString() +`\n`+
-        `Character : `+ character.map(x=>x.name).toString() +`\n`+
-        `Tags : `+ tags.map(x=>x.name).toString() +`\n`
-
-    });
 };
 
 module.exports = handleNhentaiInfo;
