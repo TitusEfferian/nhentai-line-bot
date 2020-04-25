30ff7bed116e1c7c6edb67c05a2c81c2b70d77b1
titusefferian <titus.efferian@tokopedia.com>
2020-04-25 13:50:48 +0700

    get random endpoint

--- controller/handleHelpMessage.js
+++ controller/handleHelpMessage.js
@@ -1,4 +1,9 @@
-const handleHelpMessage = (client, replyToken) => {
+const randomEndPoint = process.env.RANDOM_ENDPOINT.toString();
+const handleHelpMessage = async (client, replyToken) => {
+    const hitRandomEndPoint = await fetch(randomEndPoint);
+    const parseRandomEndPoint = await hitRandomEndPoint.json();
+    const getRandomTag = parseRandomEndPoint.data;
+
     const iconUrl = 'https://b2.crackwatch.com/file/crackwatch-temp/i2vhbw89a.jpg';
     return client.replyMessage(replyToken, {
         "type": "text",
@@ -11,7 +16,7 @@ const handleHelpMessage = (client, replyToken) => {
                     "action": {
                         "type": "message",
                         "label": "nhentai reader",
-                        "text": "g/310084"
+                        "text": getRandomTag,
                     }
                 },
                 {
