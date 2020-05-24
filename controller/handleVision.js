a8a3c55867520bf9a43e43fc0962e93c428736c7
titusefferian <titus.efferian@tokopedia.com>
2020-05-24 20:10:34 +0700

    catch handle vision

--- controller/handleVision.js
+++ controller/handleVision.js
@@ -6,21 +6,28 @@ const handleVision = async (client, event, imageName) => {
     const { replyToken, source: { groupId, type }, message: { id } } = event;
     if (type === 'group') {
         if (groupId === devGroupId || groupId === efferianGroupId) {
-            const [result] = await vision.webDetection(`gs://eff_temp/${imageName}`);
-            const webDetection = result.webDetection;
-            if (webDetection.fullMatchingImages.length) {
+            try {
+                const [result] = await vision.webDetection(`gs://eff_temp/${imageName}`);
+                const webDetection = result.webDetection;
+                if (webDetection.fullMatchingImages.length) {
+                    return client.replyMessage(replyToken, {
+                        type: 'text',
+                        text: 'vision: ' + webDetection.fullMatchingImages.map((x, y) => {
+                            return '\n' + (y + 1) + '. ' + x.url;
+                        }),
+                    });
+                }
+                if (!webDetection.fullMatchingImages.length) {
+                    return client.replyMessage(replyToken, {
+                        type: 'text',
+                        text: `no vision detected`,
+                    });
+                }
+            } catch (err) {
                 return client.replyMessage(replyToken, {
                     type: 'text',
-                    text: 'vision: ' + webDetection.fullMatchingImages.map((x, y) => {
-                        return '\n' + (y + 1) + '. ' + x.url;
-                    }),
-                });
-            }
-            if (!webDetection.fullMatchingImages.length) {
-                return client.replyMessage(replyToken, {
-                    type: 'text',
-                    text: `no vision detected`,
-                });
+                    text: JSON.stringify(err)
+                })
             }
         }
     }
