0eb033ecb7ef497676d1e510cd975b2b60a8a8ff
titusefferian <titus.efferian@tokopedia.com>
2020-05-24 15:21:54 +0700

    dev sauce carousel

---
+++ StorageConnector/index.js
@@ -0,0 +1,7 @@
+const { Storage } = require('@google-cloud/storage');
+
+const storage = new Storage({
+    keyFilename: './database.json',
+});
+
+module.exports = storage;
--- controller/handleSauce.js
+++ controller/handleSauce.js
@@ -1,8 +1,28 @@
 const devGroupId = process.env.DEV_GROUP_ID.toString();
+const storage = require('../StorageConnector');
+
+const handleLoopLength = filesLength => {
+    if(filesLength > 5) {
+       return 5; 
+    }
+    return filesLength;
+}
 
 const handleSauce = (client, event) => {
     const { replyToken, source: { groupId }, message: { id } } = event;
+    const imageResult = [];
     if (groupId === devGroupId) {
+        const [files] = await storage.bucket(`eff_temp`).getFiles();
+        for (let a = 0; a < handleLoopLength(files.length); a++) {
+            imageResult.push({
+                "imageUrl": `https://storage.cloud.google.com/eff_temp/${files[a].name}`,
+                "action": {
+                    "type": "message",
+                    "label": `${files[a].name}`,
+                    "text": `sauce_id ${files[a].name}`,
+                }
+            });
+        }
         return client.replyMessage(replyToken,[
             {
                 type: 'text',
@@ -11,6 +31,14 @@ const handleSauce = (client, event) => {
             {
                 type: 'text',
                 text: 'pilih gambar berikut yang ingin dicari sumbernya'
+            },
+            {
+                type: 'template',
+                altText: 'list of image',
+                template: {
+                    type: 'image_carouse',
+                    columns: imageResult,
+                }
             }
         ])
     }
