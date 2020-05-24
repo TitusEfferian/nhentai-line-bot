1562090a71ac01a5e57a96dc92892e90821252fa
titusefferian <titus.efferian@tokopedia.com>
2020-05-24 15:31:45 +0700

    dev sauce carousel

--- controller/handleSauce.js
+++ controller/handleSauce.js
@@ -2,8 +2,8 @@ const devGroupId = process.env.DEV_GROUP_ID.toString();
 const storage = require('../StorageConnector');
 
 const handleLoopLength = filesLength => {
-    if(filesLength > 5) {
-       return 5; 
+    if (filesLength > 5) {
+        return 5;
     }
     return filesLength;
 }
@@ -12,35 +12,42 @@ const handleSauce = async (client, event) => {
     const { replyToken, source: { groupId }, message: { id } } = event;
     const imageResult = [];
     if (groupId === devGroupId) {
-        const [files] = await storage.bucket(`eff_temp`).getFiles();
-        for (let a = 0; a < handleLoopLength(files.length); a++) {
-            imageResult.push({
-                "imageUrl": `https://storage.cloud.google.com/eff_temp/${files[a].name}`,
-                "action": {
-                    "type": "message",
-                    "label": `${files[a].name}`,
-                    "text": `sauce_id ${files[a].name}`,
+        try {
+            const [files] = await storage.bucket(`eff_temp`).getFiles();
+            for (let a = 0; a < handleLoopLength(files.length); a++) {
+                imageResult.push({
+                    "imageUrl": `https://storage.cloud.google.com/eff_temp/${files[a].name}`,
+                    "action": {
+                        "type": "message",
+                        "label": `${files[a].name}`,
+                        "text": `sauce_id ${files[a].name}`,
+                    }
+                });
+            }
+            return client.replyMessage(replyToken, [
+                {
+                    type: 'text',
+                    text: 'anda mencari sumber dari gambar?'
+                },
+                {
+                    type: 'text',
+                    text: 'pilih gambar berikut yang ingin dicari sumbernya'
+                },
+                {
+                    type: 'template',
+                    altText: 'list of image',
+                    template: {
+                        type: 'image_carousel',
+                        columns: imageResult,
+                    }
                 }
+            ])
+        } catch (err) {
+            return client.replyMessage(replyToken, {
+                type: 'text',
+                text: JSON.stringify(err),
             });
         }
-        return client.replyMessage(replyToken,[
-            {
-                type: 'text',
-                text: 'anda mencari sumber dari gambar?'
-            },
-            {
-                type: 'text',
-                text: 'pilih gambar berikut yang ingin dicari sumbernya'
-            },
-            {
-                type: 'template',
-                altText: 'list of image',
-                template: {
-                    type: 'image_carousel',
-                    columns: imageResult,
-                }
-            }
-        ])
     }
     return;
 }
