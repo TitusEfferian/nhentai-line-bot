8cb31f2f415cec9ba657e176f9b01b1748e63c44
titus efferian <titus.efferian@tokopedia.com>
2020-06-01 14:20:55 +0700

    handle error random

--- controller/handleRandomNhentai.js
+++ controller/handleRandomNhentai.js
@@ -4,82 +4,47 @@ const nhentaiByPassOriginal = process.env.NHENTAI_BYPASS_ORIGINAL.toString();
 const nhentaiFullReader = process.env.NHENTAI_FULL_READER.toString();
 
 const handleRandomNhentai = async (client, replyToken) => {
-    const hitRandomEndPoint = await fetch(randomEndPoint);
-    const parseRandomEndPoint = await hitRandomEndPoint.json();
-    const getRandomTag = parseRandomEndPoint.data.id;
-    const previewImg = parseRandomEndPoint.data.preview;
-    return client.replyMessage(replyToken, [
-        {
-            "type": "text",
-            "text": "klik gambar untuk melihat seluruh halaman",
-        },
-        {
-            "type": "template",
-            "altText": "nhentai results",
-            "template": {
-                "type": "image_carousel",
-                "columns": [
-                    {
-                        "imageUrl": `${nhentaiByPassOriginal}?source=${previewImg}`,
-                        "action": {
-                            "type": "uri",
-                            "label": getRandomTag,
-                            "uri": `${nhentaiFullReader}?source=${getRandomTag.split('/')[1]}`,
-                        }
+    try {
+        const hitRandomEndPoint = await fetch(randomEndPoint);
+        const parseRandomEndPoint = await hitRandomEndPoint.json();
+        const success = parseRandomEndPoint.success;
+        const getRandomTag = parseRandomEndPoint.data.id;
+        const previewImg = parseRandomEndPoint.data.preview;
+        if (success) {
+            return client.replyMessage(replyToken, [
+                {
+                    "type": "text",
+                    "text": "klik gambar untuk melihat seluruh halaman",
+                },
+                {
+                    "type": "template",
+                    "altText": "nhentai results",
+                    "template": {
+                        "type": "image_carousel",
+                        "columns": [
+                            {
+                                "imageUrl": `${nhentaiByPassOriginal}?source=${previewImg}`,
+                                "action": {
+                                    "type": "uri",
+                                    "label": getRandomTag,
+                                    "uri": `${nhentaiFullReader}?source=${getRandomTag.split('/')[1]}`,
+                                }
+                            }
+                        ],
                     }
-                ],
-            }
+                }
+            ]);
         }
-    ]);
-    // return client.replyMessage(replyToken,
-    //     {
-    //         "type": "flex",
-    //         "altText": "nhentai random",
-    //         "contents": {
-    //             "type": "carousel",
-    //             "contents": [
-    //                 {
-    //                     "type": "bubble",
-    //                     "size": "kilo",
-    //                     "body": {
-    //                         "type": "box",
-    //                         "layout": "vertical",
-    //                         "contents": [
-    //                             {
-    //                                 "type": "image",
-    //                                 "url": `${nhentaiByPassOriginal}?source=${previewImg}`,
-    //                                 "size": "full",
-    //                                 "aspectMode": "cover",
-    //                                 "gravity": "top",
-    //                                 "aspectRatio": "2:3",
-    //                                 "margin": "none"
-    //                             },
-    //                             {
-    //                                 "type": "box",
-    //                                 "layout": "vertical",
-    //                                 "contents": [
-    //                                     {
-    //                                         "type": "text",
-    //                                         "text": getRandomTag,
-    //                                         "color": "#ffffff",
-    //                                         "align": "center"
-    //                                     }
-    //                                 ],
-    //                                 "position": "absolute",
-    //                                 "offsetBottom": "0px",
-    //                                 "offsetStart": "0px",
-    //                                 "offsetEnd": "0px",
-    //                                 "backgroundColor": "#ed2553",
-    //                                 "paddingAll": "20px"
-    //                             }
-    //                         ],
-    //                         "paddingAll": "0px"
-    //                     }
-    //                 }
-    //             ]
-    //         }
-    //     }
-    // )
+        return client.replyMessage(replyToken, {
+            type: 'text',
+            text: 'error endpoint random'
+        });
+    } catch (err) {
+        return client.replyMessage(replyToken, {
+            type: 'text',
+            text: JSON.stringify(err),
+        });
+    }
 }
 
 module.exports = handleRandomNhentai;
