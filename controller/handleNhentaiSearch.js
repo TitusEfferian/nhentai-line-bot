801785f6028180eda055f5502003d661e9af8d23
titusefferian <titus.efferian@tokopedia.com>
2020-06-07 15:33:39 +0700

    handle popular

--- controller/handleNhentaiSearch.js
+++ controller/handleNhentaiSearch.js
@@ -9,7 +9,7 @@ const nhentaiSearchV2 = process.env.NHENTAI_SEARCH_V2.toString();
 const handleNhentaiSearch = async (searhParams, client, replyToken) => {
     try {
         const isPopular = searhParams.split(' popular');
-        const searchSanitize = searhParams.replace(' ', '%20');
+        const searchSanitize = isPopular[0].replace(' ', '%20');
         const fetchSearchCrawler = await fetch(`${nhentaiSearchV2}?search=${searchSanitize}&sort=${isPopular.length === 1 ? 'date' : 'popular'}`);
         const { arrayOfResult } = await fetchSearchCrawler.json();
 
