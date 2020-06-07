ccc75a5a2449044d91524d1b974854224e063530
titusefferian <titus.efferian@tokopedia.com>
2020-06-07 14:08:17 +0700

    quick reply

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-git fetch && git rebase origin/master && gcloud functions deploy webhook --region=asia-east2 --runtime=nodejs10 --tri
-gger-http --entry-point=webhook
+git fetch && git rebase origin/master && gcloud functions deploy webhook --region=asia-east2 --runtime=nodejs10 --trigger-http --entry-point=webhook
