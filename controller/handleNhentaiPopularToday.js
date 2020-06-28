e1724ca2684cf1d7d23f0ed37439763eb8b60247
titusefferian <kiritosato@gmail.com>
2020-06-28 22:48:13 +0700

    handle fetch nhentai popular

--- controller/handleNhentaiPopularToday.js
+++ controller/handleNhentaiPopularToday.js
@@ -1,10 +1,26 @@
 const fetch = require('isomorphic-unfetch');
 
 const handleNhentaiPopularToday = async (client, replyToken) => {
-    return client.replyMessage(replyToken, {
-        type: 'text',
-        text: 'on development',
-    })
+    try {
+        const resultPopular = await fetch('https://asia-east2-fleet-range-273715.cloudfunctions.net/popular-now');
+        const { success, arrayOfResult } = await resultPopular.json();
+        if (success) {
+            return client.replyMessage(replyToken, {
+                type: 'text',
+                text: String(success),
+            })
+        }
+        return client.replyMessage(replyToken, {
+            type: 'text',
+            text: 'success false',
+        })
+    } catch (err) {
+        return client.replyMessage(replyToken, {
+            type: 'text',
+            text: JSON.stringify(err),
+        })
+    }
+
 };
 
 module.exports = handleNhentaiPopularToday;
